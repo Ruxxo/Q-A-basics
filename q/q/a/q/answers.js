419054f6af0405d6c6ded17c4c26a87f94b6eb5c
Ruxxo <ebukaogd1@gmail.com>
2023-04-12 16:00:48 +0100

    first commit

---
+++ q/q/a/q/answers.js
@@ -0,0 +1,5 @@
+// finding Dog age using formula
+var dogAge = prompt ('How Old is your Dog');
+var humanAge = (dogAge - 2 * 4) +21 ;
+alert ('Your Dog is' + humanAge + " years old in human years");
+
