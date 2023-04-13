d0c19ac8cb253127148c18c0ecf3c35ade24f4a6
Ruxxo <ebukaogd1@gmail.com>
2023-04-13 22:50:43 +0100

    updated Again

--- q/q/a/q/answers.js
+++ q/q/a/q/answers.js
@@ -1,5 +1,32 @@
 // finding Dog age using formula
 var dogAge = prompt ('How Old is your Dog');
 var humanAge = (dogAge - 2 * 4) +21 ;
-alert ('Your Dog is' + humanAge + " years old in human years");
+alert ('Your Dog is ' + humanAge + " years old in human years");
 
+
+
+function getMilk(money) {   
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+      var numberOfBottles =(Math.floor(money /1.5));
+      console.log ("buy "+ numberOfBottles + (' bottles of Milk'));
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+  }
+  
+  
+  getMilk(5);
